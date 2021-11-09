0dddab18ea7b828a6f0d4f30d85e01d843db0ef3
Andréas J. Otto <milkyotto@gmail.com>
2021-11-10 01:14:04 +0200

    Added my first view, controller and a bootstrap theme

---
+++ mediashare/Controllers/MediaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mediashare.Models;
+
+namespace mediashare.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: Media
+        public ActionResult Index()
+        {
+            var media = new Media() {Name = "My Picture"};
+            return View(media);
+        }
+    }
+}
---
+++ mediashare/Models/Media.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mediashare.Models
+{
+    public class Media
+    {
+        public int Id {get; set; }
+        public string Name {get; set; }
+    }
+}
