8c0e73a693a2759959d27659d697c63397778c4f
Andréas J. Otto <milkyotto@gmail.com>
2021-11-13 17:44:48 +0200

    Added a View Users page.

--- mediashare/Controllers/MediaController.cs
+++ mediashare/Controllers/MediaController.cs
@@ -10,16 +10,16 @@ namespace MediaShare.Controllers
     public class MediaController : Controller
     {
         // GET: Media
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrEmpty(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
-        public ActionResult Initial()
+        public ActionResult Index()
         {
             var media = new Media() { Name = "My Media" };
 
---
+++ mediashare/Controllers/UserController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using mediashare.Models;
+
+namespace mediashare.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListUser()
+        {
+            List<UserModel> users = new List<UserModel>();
+            string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection Conn = new SqlConnection(connString))
+            {
+                Conn.Open();
+                SqlCommand sqlCom = new SqlCommand("SELECT Email FROM dbo.AspNetUsers", Conn);
+                SqlDataReader reader = sqlCom.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    UserModel USER = new UserModel();
+                    USER.UserName = (string) reader["Email"];
+                    users.Add(USER);
+                }
+
+                Conn.Close();
+            }
+
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
