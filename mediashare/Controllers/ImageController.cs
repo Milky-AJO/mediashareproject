479a273e29d452984320a74838f60b9980036fbe
Andréas J. Otto <milkyotto@gmail.com>
2021-11-13 22:37:31 +0200

    Image testing

---
+++ mediashare/Controllers/ImageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MediaShare.Models;
+
+namespace mediashare.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Add(Image image)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ mediashare/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mediashare.Models
+{
+    public class ImageModel
+    {
+        public int ImageId { get; set; }
+        public string ImageTitle { get; set; }
+        public string ImagePath { get; set; }
+        public bool ImagePublic { get; set; }
+    }
+}
